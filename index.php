f5b625628736d68571acbb99f57dfe5c8be1cc17
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2014-05-27 20:16:40 +0300

    Added index file with basic file uploader
    Copied the file uploader from another project I was working on.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>SGL WebApp</title>
+ </head>
+ <body>
+  <div align="center">
+  <h4> File uploads </h4>
+
+  <table>
+  <form enctype="multipart/form-data" action="" method="post"> <!--TODO: Use JSON to upload file -->
+  <tr><td>
+  Select File:</td><td>
+  <input type="file" size="35" name="uploadedfile" /></td></tr><tr>
+  </table>
+  <input type="submit" name="Upload" value="Upload" />
+  </p>
+  </form>
+
+
+  </div>
+ </body>
+</html>
+
